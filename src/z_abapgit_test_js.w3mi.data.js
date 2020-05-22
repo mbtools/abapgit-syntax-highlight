31abe838ba1d240cee4e5c401d67732fce4f4aa9
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2020-05-22 13:39:01 +0200

    Update z_abapgit_test_js.w3mi.data.js

--- src/z_abapgit_test_js.w3mi.data.js
+++ src/z_abapgit_test_js.w3mi.data.js
@@ -8,11 +8,11 @@ var variable_2 = 'also text';
 var variable_3 = Array( true, false );
 
 /* next line was added */
+if (0 == 1) { variable = true; }
 
 /* next line was changed */
-if (0 == 1) { variable = true; }
+if (0 == 1) { variable = false; }
 
 /* next line was removed */
-if (0 == 1) { variable = true; }
 
 /* eof */
